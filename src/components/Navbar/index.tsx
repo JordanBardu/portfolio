2e1522202eb6133d6768c56746755437d87ea4c7
Jordan Bardu <jordan.bardu.etu@univ-lille.fr>
2026-03-18 10:37:29 +0100

    Added liquid glass styling to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,8 +3,8 @@ import MenuButton from "../MenuButton";
 
 function Navbar() {
   return (
-    <nav>
-      <ul className="fixed top-0 left-0 w-full z-50 flex flex-row items-start px-6 justify-center gap-24">
+    <nav className="fixed top-0 left-0 w-full z-50 flex justify-center">
+      <ul className="mt-4 flex flex-row items-start gap-24 px-8 rounded-2xl backdrop-blur-xl bg-white/10 border border-white/20 shadow-[0_8px_32px_rgba(0,0,0,0.25)]">
         {buttonsContent.map((button) => (
           <MenuButton route={button.route} label={button.label} />
         ))}
